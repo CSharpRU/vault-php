dff0e4ea1be246700e6451a6acd1dfb7752682da
Yaroslav Lukyanov <c_sharp@mail.ru>
2017-08-02 18:01:15 +0700

    split client into two classes, but still need to refactor it

--- src/Client.php
+++ src/Client.php
@@ -3,37 +3,22 @@
 namespace Vault;
 
 use Cache\Adapter\Common\CacheItem;
-use GuzzleHttp\Exception\TransferException;
-use GuzzleHttp\Psr7\Request;
 use Psr\Cache\CacheItemPoolInterface;
-use Psr\Http\Message\RequestInterface;
 use Psr\Http\Message\ResponseInterface;
-use Psr\Log\LoggerAwareInterface;
-use Psr\Log\LoggerAwareTrait;
-use Psr\Log\LoggerInterface;
-use Psr\Log\NullLogger;
 use Vault\AuthenticationStrategies\AuthenticationStrategy;
-use Vault\Builders\ResponseBuilder;
-use Vault\Exceptions\ClientException;
 use Vault\Exceptions\DependencyException;
-use Vault\Exceptions\ServerException;
 use Vault\Helpers\ModelHelper;
 use Vault\Models\Token;
 use Vault\ResponseModels\Response;
-use Vault\Transports\Transport;
 
 /**
  * Class Client
  *
  * @package Vault
  */
-class Client implements LoggerAwareInterface
+class Client extends BaseClient
 {
-    const VERSION_1 = 'v1';
-
     const TOKEN_CACHE_KEY = 'token';
-
-    use LoggerAwareTrait;
 
     /**
      * Threshold of re-authentication attempts.
@@ -41,21 +26,6 @@ class Client implements LoggerAwareInterface
      * @var int
      */
     protected $reAuthenticationThreshold = 3;
-
-    /**
-     * @var string
-     */
-    protected $version = self::VERSION_1;
-
-    /**
-     * @var Token
-     */
-    protected $token;
-
-    /**
-     * @var Transport
-     */
-    protected $transport;
 
     /**
      * @var CacheItemPoolInterface
@@ -68,31 +38,12 @@ class Client implements LoggerAwareInterface
     protected $authenticationStrategy;
 
     /**
-     * @var ResponseBuilder
-     */
-    protected $responseBuilder;
-
-    /**
      * @var int
      */
     private $reAuthenticationCounter = 0;
 
     /**
-     * Client constructor.
-     *
-     * @param Transport       $transport
-     * @param LoggerInterface $logger
-     */
-    public function __construct(Transport $transport, LoggerInterface $logger = null)
-    {
-        $this->transport = $transport;
-        $this->logger = $logger ?: new NullLogger();
-        $this->responseBuilder = new ResponseBuilder();
-    }
-
-    /**
-     * @param string $url
-     * @param array  $options
+     * @param string $path
      *
      * @return Response
      *
@@ -102,67 +53,120 @@ class Client implements LoggerAwareInterface
      * @throws \RuntimeException
      * @throws \InvalidArgumentException
      */
-    public function head($url, array $options = [])
+    public function read($path)
     {
-        return $this->responseBuilder->build($this->send(new Request('HEAD', $url), $options));
+        return $this->get($this->buildPath($path));
     }
 
     /**
-     * @param RequestInterface $request
-     * @param array            $options
+     * @param string $path
      *
-     * @return ResponseInterface
+     * @return string
+     */
+    public function buildPath($path)
+    {
+        if (!$this->version) {
+            $this->logger->warning('API version is not set!');
+
+            return $path;
+        }
+
+        return sprintf('/%s%s', $this->version, $path);
+    }
+
+    /**
+     * @param string $path
+     * @param array  $data
      *
-     * @throws \Psr\Cache\InvalidArgumentException
+     * @return Response
+     *
      * @throws \Vault\Exceptions\TransportException
-     * @throws \Vault\Exceptions\ClientException
      * @throws \Vault\Exceptions\ServerException
+     * @throws \Vault\Exceptions\ClientException
      * @throws \RuntimeException
      * @throws \InvalidArgumentException
      */
-    public function send(RequestInterface $request, array $options = [])
+    public function write($path, array $data = [])
     {
-        $request = $request->withHeader('User-Agent', 'VaultPHP/1.0.0');
-        $request = $request->withHeader('Content-Type', 'application/json');
+        return $this->post($this->buildPath($path), ['body' => json_encode($data)]);
+    }
 
-        if ($this->token) {
-            $request = $request->withHeader('X-Vault-Token', $this->token->getAuth()->getClientToken());
-        }
+    /**
+     * @param string $path
+     *
+     * @return Response
+     *
+     * @throws \Vault\Exceptions\TransportException
+     * @throws \Vault\Exceptions\ServerException
+     * @throws \Vault\Exceptions\ClientException
+     * @throws \RuntimeException
+     * @throws \InvalidArgumentException
+     */
+    public function revoke($path)
+    {
+        return $this->delete($this->buildPath($path));
+    }
 
-        $this->logger->debug('Request.', [
-            'method' => $request->getMethod(),
-            'uri' => $request->getUri(),
-            'headers' => $request->getHeaders(),
-            'body' => $request->getBody()->getContents(),
-        ]);
+    /**
+     * @return CacheItemPoolInterface
+     */
+    public function getCache()
+    {
+        return $this->cache;
+    }
 
-        try {
-            $response = $this->transport->send($request, $options);
-        } catch (TransferException $e) {
-            $this->logger->error('Something went wrong when calling Vault.', [
-                'code' => $e->getCode(),
-                'message' => $e->getMessage(),
-            ]);
+    /**
+     * @param CacheItemPoolInterface $cache
+     *
+     * @return $this
+     */
+    public function setCache($cache)
+    {
+        $this->cache = $cache;
 
-            $this->logger->debug('Trace.', ['exception' => $e]);
+        return $this;
+    }
 
-            throw new ServerException(sprintf('Something went wrong when calling Vault (%s).', $e->getMessage()));
-        }
+    /**
+     * @return AuthenticationStrategy
+     */
+    public function getAuthenticationStrategy()
+    {
+        return $this->authenticationStrategy;
+    }
 
-        $this->logger->debug('Response.', [
-            'statusCode' => $response->getStatusCode(),
-            'reasonPhrase' => $response->getReasonPhrase(),
-            'headers ' => $response->getHeaders(),
-            'body' => $response->getBody()->getContents(),
-        ]);
+    /**
+     * @param AuthenticationStrategy $authenticationStrategy
+     *
+     * @return $this
+     */
+    public function setAuthenticationStrategy(AuthenticationStrategy $authenticationStrategy)
+    {
+        $authenticationStrategy->setClient($this);
 
-        if ($this->isNeedToRetryRequest($response)) {
-            return $this->send($request, $options);
-        }
+        $this->authenticationStrategy = $authenticationStrategy;
 
-        $this->checkResponse($response);
+        return $this;
+    }
 
-        return $response;
+    /**
+     * @return int
+     */
+    public function getReAuthenticationThreshold()
+    {
+        return $this->reAuthenticationThreshold;
+    }
+
+    /**
+     * @param int $reAuthenticationThreshold
+     *
+     * @return $this
+     */
+    public function setReAuthenticationThreshold($reAuthenticationThreshold)
+    {
+        $this->reAuthenticationThreshold = $reAuthenticationThreshold > 0 ? $reAuthenticationThreshold : 0;
+
+        return $this;
     }
 
     /**
@@ -185,7 +189,7 @@ class Client implements LoggerAwareInterface
     /**
      * @return bool
      */
-    private function isReAuthenticationInProgress()
+    protected function isReAuthenticationInProgress()
     {
         return $this->reAuthenticationCounter > 0;
     }
@@ -195,7 +199,7 @@ class Client implements LoggerAwareInterface
      *
      * @return bool
      */
-    private function isTokenExpired($token)
+    protected function isTokenExpired($token)
     {
         return !$token ||
             (
@@ -338,23 +342,6 @@ class Client implements LoggerAwareInterface
     }
 
     /**
-     * @param string $url
-     * @param array  $options
-     *
-     * @return Response
-     *
-     * @throws \Vault\Exceptions\TransportException
-     * @throws \Vault\Exceptions\ServerException
-     * @throws \Vault\Exceptions\ClientException
-     * @throws \RuntimeException
-     * @throws \InvalidArgumentException
-     */
-    public function get($url = null, array $options = [])
-    {
-        return $this->responseBuilder->build($this->send(new Request('GET', $url), $options));
-    }
-
-    /**
      * @TODO: move to separated class
      *
      * @return bool
@@ -377,337 +364,5 @@ class Client implements LoggerAwareInterface
     private function resetReAuthenticationCounter()
     {
         $this->reAuthenticationCounter = 0;
-    }
-
-    /**
-     * Returns true whenever request should be retried.
-     *
-     * @param ResponseInterface $response
-     *
-     * @throws \Vault\Exceptions\ClientException
-     * @throws \Vault\Exceptions\ServerException
-     */
-    protected function checkResponse(ResponseInterface $response)
-    {
-        if ($response->getStatusCode() >= 400) {
-            $message = sprintf(
-                "Something went wrong when calling Vault (%s - %s)\n%s.",
-                $response->getStatusCode(),
-                $response->getReasonPhrase(),
-                $response->getBody()->getContents()
-            );
-
-            if ($response->getStatusCode() >= 500) {
-                throw new ServerException($message, $response->getStatusCode(), $response);
-            }
-
-            throw new ClientException($message, $response->getStatusCode(), $response);
-        }
-    }
-
-    /**
-     * @param string $url
-     * @param array  $options
-     *
-     * @return Response
-     *
-     * @throws \Vault\Exceptions\TransportException
-     * @throws \Vault\Exceptions\ServerException
-     * @throws \Vault\Exceptions\ClientException
-     * @throws \RuntimeException
-     * @throws \InvalidArgumentException
-     */
-    public function put($url, array $options = [])
-    {
-        return $this->responseBuilder->build($this->send(new Request('PUT', $url), $options));
-    }
-
-    /**
-     * @param string $url
-     * @param array  $options
-     *
-     * @return Response
-     *
-     * @throws \Vault\Exceptions\TransportException
-     * @throws \Vault\Exceptions\ServerException
-     * @throws \Vault\Exceptions\ClientException
-     * @throws \RuntimeException
-     * @throws \InvalidArgumentException
-     */
-    public function patch($url, array $options = [])
-    {
-        return $this->responseBuilder->build($this->send(new Request('PATCH', $url), $options));
-    }
-
-    /**
-     * @param string $url
-     * @param array  $options
-     *
-     * @return Response
-     *
-     * @throws \Vault\Exceptions\TransportException
-     * @throws \Vault\Exceptions\ServerException
-     * @throws \Vault\Exceptions\ClientException
-     * @throws \RuntimeException
-     * @throws \InvalidArgumentException
-     */
-    public function options($url, array $options = [])
-    {
-        return $this->responseBuilder->build($this->send(new Request('OPTIONS', $url), $options));
-    }
-
-    /**
-     * @param string $path
-     *
-     * @return Response
-     *
-     * @throws \Vault\Exceptions\TransportException
-     * @throws \Vault\Exceptions\ServerException
-     * @throws \Vault\Exceptions\ClientException
-     * @throws \RuntimeException
-     * @throws \InvalidArgumentException
-     */
-    public function read($path)
-    {
-        return $this->get($this->buildPath($path));
-    }
-
-    /**
-     * @param string $path
-     *
-     * @return string
-     */
-    public function buildPath($path)
-    {
-        if (!$this->version) {
-            $this->logger->warning('API version is not set!');
-
-            return $path;
-        }
-
-        return sprintf('/%s%s', $this->version, $path);
-    }
-
-    /**
-     * @param string $path
-     * @param array  $data
-     *
-     * @return Response
-     *
-     * @throws \Vault\Exceptions\TransportException
-     * @throws \Vault\Exceptions\ServerException
-     * @throws \Vault\Exceptions\ClientException
-     * @throws \RuntimeException
-     * @throws \InvalidArgumentException
-     */
-    public function write($path, array $data = [])
-    {
-        return $this->post($this->buildPath($path), ['body' => json_encode($data)]);
-    }
-
-    /**
-     * @param string $url
-     * @param array  $options
-     *
-     * @return Response
-     *
-     * @throws \Vault\Exceptions\TransportException
-     * @throws \Vault\Exceptions\ServerException
-     * @throws \Vault\Exceptions\ClientException
-     * @throws \RuntimeException
-     * @throws \InvalidArgumentException
-     */
-    public function post($url, array $options = [])
-    {
-        return $this->responseBuilder->build($this->send(new Request('POST', $url), $options));
-    }
-
-    /**
-     * @param string $path
-     *
-     * @return Response
-     *
-     * @throws \Vault\Exceptions\TransportException
-     * @throws \Vault\Exceptions\ServerException
-     * @throws \Vault\Exceptions\ClientException
-     * @throws \RuntimeException
-     * @throws \InvalidArgumentException
-     */
-    public function revoke($path)
-    {
-        return $this->delete($this->buildPath($path));
-    }
-
-    /**
-     * @param string $url
-     * @param array  $options
-     *
-     * @return Response
-     *
-     * @throws \Vault\Exceptions\TransportException
-     * @throws \Vault\Exceptions\ServerException
-     * @throws \Vault\Exceptions\ClientException
-     * @throws \RuntimeException
-     * @throws \InvalidArgumentException
-     */
-    public function delete($url, array $options = [])
-    {
-        return $this->responseBuilder->build($this->send(new Request('DELETE', $url), $options));
-    }
-
-    /**
-     * @return string
-     */
-    public function getVersion()
-    {
-        return $this->version;
-    }
-
-    /**
-     * @param string $version
-     *
-     * @return $this
-     */
-    public function setVersion($version)
-    {
-        $this->version = $version;
-
-        return $this;
-    }
-
-    /**
-     * @return Token
-     */
-    public function getToken()
-    {
-        return $this->token;
-    }
-
-    /**
-     * @param Token $token
-     *
-     * @return $this
-     */
-    public function setToken(Token $token)
-    {
-        $this->token = $token;
-
-        return $this;
-    }
-
-    /**
-     * @return Transport
-     */
-    public function getTransport()
-    {
-        return $this->transport;
-    }
-
-    /**
-     * @param Transport $transport
-     *
-     * @return $this
-     */
-    public function setTransport($transport)
-    {
-        $this->transport = $transport;
-
-        return $this;
-    }
-
-    /**
-     * Sets a logger.
-     *
-     * @param LoggerInterface $logger
-     *
-     * @return $this
-     */
-    public function setLogger(LoggerInterface $logger)
-    {
-        $this->logger = $logger;
-
-        return $this;
-    }
-
-    /**
-     * @return CacheItemPoolInterface
-     */
-    public function getCache()
-    {
-        return $this->cache;
-    }
-
-    /**
-     * @param CacheItemPoolInterface $cache
-     *
-     * @return $this
-     */
-    public function setCache($cache)
-    {
-        $this->cache = $cache;
-
-        return $this;
-    }
-
-    /**
-     * @return AuthenticationStrategy
-     */
-    public function getAuthenticationStrategy()
-    {
-        return $this->authenticationStrategy;
-    }
-
-    /**
-     * @param AuthenticationStrategy $authenticationStrategy
-     *
-     * @return $this
-     */
-    public function setAuthenticationStrategy(AuthenticationStrategy $authenticationStrategy)
-    {
-        $authenticationStrategy->setClient($this);
-
-        $this->authenticationStrategy = $authenticationStrategy;
-
-        return $this;
-    }
-
-    /**
-     * @return ResponseBuilder
-     */
-    public function getResponseBuilder()
-    {
-        return $this->responseBuilder;
-    }
-
-    /**
-     * @param ResponseBuilder $responseBuilder
-     *
-     * @return $this
-     */
-    public function setResponseBuilder(ResponseBuilder $responseBuilder)
-    {
-        $this->responseBuilder = $responseBuilder;
-
-        return $this;
-    }
-
-    /**
-     * @return int
-     */
-    public function getReAuthenticationThreshold()
-    {
-        return $this->reAuthenticationThreshold;
-    }
-
-    /**
-     * @param int $reAuthenticationThreshold
-     *
-     * @return $this
-     */
-    public function setReAuthenticationThreshold($reAuthenticationThreshold)
-    {
-        $this->reAuthenticationThreshold = $reAuthenticationThreshold > 0 ? $reAuthenticationThreshold : 0;
-
-        return $this;
     }
 }
