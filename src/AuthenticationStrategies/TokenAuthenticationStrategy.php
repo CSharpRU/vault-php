b9c073d97c382fa68e1e345903e906641d74a0f3
Nick Santamaria <nick@nicksantamaria.net>
2018-05-01 21:38:59 +1000

    Code style fixes.

--- src/AuthenticationStrategies/TokenAuthenticationStrategy.php
+++ src/AuthenticationStrategies/TokenAuthenticationStrategy.php
@@ -11,34 +11,34 @@ use Vault\ResponseModels\Auth;
  */
 class TokenAuthenticationStrategy extends AbstractAuthenticationStrategy
 {
-  /**
-   * @var string
-   */
-  protected $token;
+    /**
+     * @var string
+     */
+    protected $token;
 
-  /**
-   * TokenAuthenticationStrategy constructor.
-   *
-   * @param string $token
-   */
-  public function __construct($token)
-  {
-    $this->token = $token;
-  }
+    /**
+     * TokenAuthenticationStrategy constructor.
+     *
+     * @param string $token
+     */
+    public function __construct($token)
+    {
+        $this->token = $token;
+    }
 
-  /**
-   * Returns auth for further interactions with Vault.
-   *
-   * @return Auth
-   * @throws \Vault\Exceptions\TransportException
-   *
-   * @throws \Vault\Exceptions\ServerException
-   * @throws \Vault\Exceptions\ClientException
-   * @throws \RuntimeException
-   * @throws \InvalidArgumentException
-   */
-  public function authenticate()
-  {
-    return new Auth(['clientToken' => $this->token]);
-  }
+    /**
+     * Returns auth for further interactions with Vault.
+     *
+     * @return Auth
+     * @throws \Vault\Exceptions\TransportException
+     *
+     * @throws \Vault\Exceptions\ServerException
+     * @throws \Vault\Exceptions\ClientException
+     * @throws \RuntimeException
+     * @throws \InvalidArgumentException
+     */
+    public function authenticate()
+    {
+        return new Auth(['clientToken' => $this->token]);
+    }
 }
