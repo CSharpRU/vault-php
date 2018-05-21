7a7376fe92ee33fe8ca15e728a6bbff1859bd789
Yaroslav Lukyanov <c_sharp@mail.ru>
2018-05-21 14:05:32 +0700

    add test for token strategy make doctrine/inflector constraint less strict

---
+++ tests/unit/AuthenticationStrategies/TokenAuthenticationStrategyTest.php
@@ -0,0 +1,55 @@
+<?php
+
+use Psr\Log\NullLogger;
+use Vault\AuthenticationStrategies\TokenAuthenticationStrategy;
+use Vault\Client;
+use VaultTransports\Guzzle6Transport;
+
+class TokenAuthenticationStrategyTest extends \Codeception\Test\Unit
+{
+    /**
+     * @var \UnitTester
+     */
+    protected $tester;
+
+    public function testCanAuthenticate()
+    {
+        $client = (new Client(new Guzzle6Transport()))
+            ->setAuthenticationStrategy(new TokenAuthenticationStrategy('db02de05-fa39-4855-059b-67221c5c2f63'))
+            ->setLogger(new NullLogger());
+
+        $this->assertEquals($client->getAuthenticationStrategy()->getClient(), $client);
+        $this->assertTrue($client->authenticate());
+        $this->assertNotEmpty($client->getToken());
+
+        return $client;
+    }
+
+    protected function setUp()
+    {
+        \VCR\VCR::turnOn();
+
+        \VCR\VCR::insertCassette('authentication-strategies/token');
+
+        return parent::setUp();
+    }
+
+    protected function tearDown()
+    {
+        // To stop recording requests, eject the cassette
+        \VCR\VCR::eject();
+
+        // Turn off VCR to stop intercepting requests
+        \VCR\VCR::turnOff();
+
+        parent::tearDown();
+    }
+
+    protected function _before()
+    {
+    }
+
+    protected function _after()
+    {
+    }
+}
